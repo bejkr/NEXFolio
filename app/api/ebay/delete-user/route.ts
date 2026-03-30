fc29efa48305cfaab30511fd87bc3e39ca7cbc6f
Boris Konecny <bkonecny45@gmail.com>
2026-03-30 11:04:50 +0200

    .

--- app/api/ebay/delete-user/route.ts
+++ app/api/ebay/delete-user/route.ts
@@ -1,3 +1,7 @@
+export async function GET(req: Request) {
+    return new Response('OK', { status: 200 });
+}
+
 export async function POST(req: Request) {
     return new Response('OK', { status: 200 });
 }
